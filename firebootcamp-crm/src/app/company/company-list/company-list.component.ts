c094fa3d72c7eecbb7b1a00b74aa684ae7028fa6
Brendan Richards <brendan_richards_msdn@hotmail.com>
2018-07-12 12:51:11 +1000

    added async pipe

--- firebootcamp-crm/src/app/company/company-list/company-list.component.ts
+++ firebootcamp-crm/src/app/company/company-list/company-list.component.ts
@@ -1,28 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Company } from 'src/app/company/company';
 import { CompanyService } from '../company.service';
 import { HttpClient } from '@angular/common/http';
+import { Subscription, Observable } from 'rxjs';
+import {takeWhile} from 'rxjs/operators';
 
 @Component({
   selector: 'fbc-company-list',
   templateUrl: './company-list.component.html',
   styleUrls: ['./company-list.component.scss']
 })
-export class CompanyListComponent implements OnInit {
+export class CompanyListComponent implements OnInit, OnDestroy  {
+
 
 
   constructor(
     private companyService: CompanyService,
-    private httpClient: HttpClient
   ) {}
 
-
-  companies: Company[];
+  componentExists = true;
+  companies$: Observable<Company[]>;
 
   ngOnInit() {
-    this.companyService.getCompanies()
-      .subscribe(next => this.companies = next);
+    this.companies$ = this.companyService.getCompanies();
   }
 
+
+  ngOnDestroy(): void {
+    this.componentExists = false;
+  }
 
 }
