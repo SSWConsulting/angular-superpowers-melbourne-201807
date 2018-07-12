9888924022aeedd340b3fd6ac0786c082560c6f8
JeanThirion <jean.thirion@esial.net>
2018-07-12 16:45:39 +1000

    spec

--- firebootcamp-crm/src/app/app.component.spec.ts
+++ firebootcamp-crm/src/app/app.component.spec.ts
@@ -1,31 +1,17 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'fbc'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('fbc');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to firebootcamp-crm!');
-  }));
+import { AppComponent } from "src/app/app.component";
+
+describe('App Component', () => {
+  it('1+1=2', () =>{
+    expect(1+1).toEqual(2);
+  })
+
+  // it('1+1=3 (this is supposed to fail)', () =>{
+  //   expect(1+1).toEqual(3);
+  // })
+
+  it('title equals Melbourne', () => {
+    const appComponent = new AppComponent();
+
+    expect(appComponent.title).toEqual("Melbourne");
+  })
 });
