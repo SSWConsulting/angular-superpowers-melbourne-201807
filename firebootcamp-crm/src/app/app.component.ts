7cbe5fc8c6c9e787c21f614064fd6bb5e3898c8a
Brendan Richards <brendan_richards_msdn@hotmail.com>
2018-07-12 11:03:56 +1000

    appComponent

--- firebootcamp-crm/src/app/app.component.ts
+++ firebootcamp-crm/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fbc';
+
+  keyPressed($event) {
+    // console.log($event);
+    this.title = $event.target.value;
+  }
+
 }
